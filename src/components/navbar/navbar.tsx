8afcca65076b2f8e4a50794e5b09b51fe16d3672
parshviSri <parshvisri@gmail.com>
2023-05-01 16:58:46 +0530

    adding tailwind css
    Signed-off-by: parshviSri <parshvisri@gmail.com>

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./navbar.css";
+function NavBar() {
+  return (
+    <>
+      <div className="text-4xl font-bold">Nav</div>
+    </>
+  );
+}
+
+export default NavBar;
